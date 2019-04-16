662f7927169b4d0a2d8147b0773798b8c51e295e
Mike Vormwald <mvormwald@gmail.com>
2019-04-16 11:43:04 -0400

    remove dependency on syracuseio css

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,6 +13,8 @@
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
 
+import css from "../css/app.css"
+
 // Import local files
 //
 // Local files can be imported directly using relative
